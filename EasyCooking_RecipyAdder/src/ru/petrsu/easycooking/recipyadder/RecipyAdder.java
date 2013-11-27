66c8d8b8288cd85b80e8a0eeeb6de7578fd9d09c
Anton Andreev <fokan.rus@gmail.com>
2013-11-27 10:58:30 +0400

    Added methods to open and close database

--- EasyCooking_RecipyAdder/src/ru/petrsu/easycooking/recipyadder/RecipyAdder.java
+++ EasyCooking_RecipyAdder/src/ru/petrsu/easycooking/recipyadder/RecipyAdder.java
@@ -2,22 +2,55 @@ package ru.petrsu.easycooking.recipyadder;
 
 import javax.swing.JFrame;
 
+import java.sql.*;
+
 public class RecipyAdder extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
-	public RecipyAdder(){
+
+	Connection c;
+	Statement stmt;
+
+	public RecipyAdder() {
 		super("EasyCooking RecipyAdder");
 	}
 
-	public void init(){
+	public void init() {
 		setBounds(100, 100, 400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c = null;
+		stmt = null;
 	}
-	
+
 	public static void main(String[] args) {
-		RecipyAdder app = new RecipyAdder(); 
-		app.init(); 
-	    app.setVisible(true); 
+		RecipyAdder app = new RecipyAdder();
+		app.init();
+		app.setVisible(true);
+		
+		app.openDB();
+		app.closeDB();
+	}
+
+	private void openDB() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			stmt = c.createStatement();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
+
+	private void closeDB() {
+		try {
+			stmt.close();
+			c.close();
+		} catch (SQLException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Database closed successfully");
 	}
 }
