d5bbea5649ef02acfb4998780a933141e104af37
Anton Andreev <fokan.rus@gmail.com>
2013-12-24 00:03:34 +0400

    Fixes. Border added to minirec

--- EasyCooking/assets/main.js
+++ EasyCooking/assets/main.js
@@ -6,19 +6,25 @@ var drawMini = function (t_id) {
 	for(var i=1;i<=len;i++)
 		ingr_txt[i]=dbProvider.getIngredientName(parseInt(ingr_lst[i]));
 	
-	var nm=dbProvider.getRecName(t_id);
+	var nm=dbProvider.getRecipeName(t_id);
 	
 	var t=document.createElement('div');
 	
-	t.attr("width", "90%");
-	t.click(function(){
+	//t.attr("width", "90%");
+	t.setAttribute("width", "90%")
+	t.onclick = function(){
 		fullPage(t_id);	
-	});
+	}
 	var str=nm.toString()+"\n";
-	for(var i=1;i<=len;i++)
-		str+=ingr_txt[i]+' ';
+	str+="<";
+	for(var i=1;i<=len;i++){
+		str+=ingr_txt[i];
+		if(i!=len) str+=", "
+	}
+	str+=">";
 	t.innerHTML=str;
-	document.getElemetById('mainSection').appendChild(t);
+	t.className="miniRec";
+	document.getElementById('mainSection').appendChild(t);
 	
 }
 var cookButtonHandler=function(){
@@ -41,7 +47,10 @@ var cookButtonHandler=function(){
 
 var allRecipesHandler = function() {
 	var mnSctn=document.getElementById('mainSection');
-	mnSctn.empty();
+	//mnSctn.empty();
+	var last;
+    while (last = mnSctn.lastChild) 
+    	mnSctn.removeChild(last);
 	var lst=dbProvider.getRecipes().split(' ');
 	var t=parseInt(lst[0]);
 	for(var i=1;i<=t;i++)
@@ -49,7 +58,10 @@ var allRecipesHandler = function() {
 };
 var favouriteHandler = function () {
 	var mnSctn=document.getElementById('mainSection');
-	mnSctn.empty();
+	//mnSctn.empty();
+	var last;
+    while (last = mnSctn.lastChild) 
+    	mnSctn.removeChild(last);
 	var lst=dbProvider.getFavouriteList().split('');
 	var t=parseInt(lst[0]);
 	for(var i=1;i<=t;i++)
@@ -59,7 +71,10 @@ var favouriteHandler = function () {
 
 var toBuyHandler = function () {
 	var mnSctn=document.getElementById('mainSection');
-	mnSctn.empty();
+	//mnSctn.empty();
+	var last;
+    while (last = mnSctn.lastChild) 
+    	mnSctn.removeChild(last);
 	var lst=dbProvider.getBuyList().split(' ');
 	var t=parseInt(lst[0]);
 	var str;
@@ -101,47 +116,51 @@ var searchHandler = function () {
 };
 
 var fullPage = function (t_id) {
+	console.log("fullpaging "+t_id);
 	var mnSctn=document.getElementById('mainSection');
-	mnSctn.empty();
+	//mnSctn.empty();
+	var last;
+    while (last = mnSctn.lastChild) 
+    	mnSctn.removeChild(last);
 	var ingr_lst=(dbProvider.getRecipeIngredients(t_id)).split(' ');
-	var nm=dbProvider.getRecName(t_id);
-	var dcr=dpProvider.getDescription(t_id);
+	var nm=dbProvider.getRecipeName(t_id);
+	var dcr=dbProvider.getDescription(t_id);
 	
 	
 	var tmp=document.createElement('div');
-	tmp.attr("scroll", "yes");
+	tmp.setAttribute("scroll", "yes");
 	
 	var tl=document.createElement('h1');
-	t1.attr("font-size","20px");
-	t1.innerHTML=mn;
-	tmp.append(t1);
+	tl.setAttribute("font-size","20px");
+	tl.innerHTML=nm;
+	tmp.appendChild(tl);
 	
 	var tgs=document.createElement('p');
 	tgs.innerHTML="tags: ";
-	tgs.attr("font-size","12px");
-	tgs.attr("align","right");
+	tgs.setAttribute("font-size","12px");
+	tgs.setAttribute("align","right");
 	
-	var tgs_lst=dpProvider.getRecipeTags(t_id);
+	var tgs_lst=dbProvider.getRecipeTags(t_id).split(' ');
 	for(var i=1;i<=tgs_lst[0];i++)
-		tgs.innerHTML+=dpProvider.getTagName(tgs_lst[i]+ " ");
-	tmp.append(tgs);
+		tgs.innerHTML+=dbProvider.getTagName(tgs_lst[i])+" ";
+	tmp.appendChild(tgs);
 	
 	
 	var p=document.createElement('p');
-	p.attr("font-size","14");
+	p.setAttribute("font-size","14");
 	for(var i=1;i<=ingr_lst[0];i++)
-		p.innerHTML+=dpProvider.getIngredientName(ingr_lst[i])+" ";
-	tmp.append(p);
+		p.innerHTML+=dbProvider.getIngredientName(ingr_lst[i])+" ";
+	tmp.appendChild(p);
 	
 	
 	
 	var d=document.createElement('p');
-	d.attr("font-size","14");
-	d.innerHTML=dcrp;
-	tmp.append(d);
+	d.setAttribute("font-size","14");
+	d.innerHTML=dcr;
+	tmp.appendChild(d);
 	
 	
-	
+	document.getElementById('mainSection').appendChild(tmp);
 	
 };
 
